ec650403e226ca088bdbc45b8ce5b531697dc6ef
Krae <kraewind@gmail.com>
2021-03-15 01:09:06 -0400

    commit tree 0ea0d68ce9b6c9b383f4102647b96497de3c32cd

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :doctors
-  resources :patients
   resources :appointments, only: [:show]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :doctors, only: [:show]
+  resources :patients, only: [:show, :index]
 end
